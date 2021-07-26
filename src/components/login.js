ff068dd9b765a037a5c3802b8277d0658c1e5222
Christian Caracach <christiancaracach@gmail.com>
2021-07-26 10:42:37 -0300

    Mejora del componente login, imagen, texto

--- src/components/login.js
+++ src/components/login.js
@@ -2,6 +2,7 @@ import React from "react";
 //Importamos Firebase
 import firebase from "firebase/app";
 import { auth } from "../firebase";
+import HomeImg from "../img/img1.png";
 
 const Login = () => {
   //Ésta función sirve para iniciar sesión con Google
@@ -11,11 +12,35 @@ const Login = () => {
 
   return (
     <div className="container text-center border border-2 rounded my-3 p-3 w-50 h-50">
-      <img src="../img/logo.png" alt="alt" className="img-fluid" />{" "}
-      {/*Todavía hay un error con las img, para solucionar mas adelante*/}
+      <div className="row my-2">
+        <div className="col-5">
+          <img
+            src={HomeImg}
+            alt="Imagen Home"
+            className="img-fluid d-block mx-auto"
+          />
+        </div>
+        <div className="col-7">
+          <p className="lead display-6">ADMINSTORE</p>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">
+              <p className="small">Llevá un control de tus ventas</p>
+            </li>
+            <li className="list-group-item">
+              <p className="small">
+                Registrá tus proveedores y las compras que hacés
+              </p>
+            </li>
+            <li className="list-group-item">
+              <p className="small">Visualizá estadísticas de tu negocio</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+
       <button
         type="button"
-        className="btn btn-success btn-lg my-2"
+        className="btn btn-success btn-lg my-2 d-block mx-auto"
         onClick={loguearConGoogle}
       >
         Ingresar
